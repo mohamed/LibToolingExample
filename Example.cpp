cbcf1739eeae7931b815cde13944ffbf5be56a3c
Mohamed A. Bamakhrama <mohamedb@synopsys.com>
2019-07-25 15:57:41 +0200

    Use LLVM 7 and CMake
    Refactored to use LLVM 7 and CMake

--- Example.cpp
+++ Example.cpp
@@ -1,14 +1,14 @@
-#include "clang/Driver/Options.h"
 #include "clang/AST/AST.h"
-#include "clang/AST/ASTContext.h"
 #include "clang/AST/ASTConsumer.h"
+#include "clang/AST/ASTContext.h"
 #include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Driver/Options.h"
 #include "clang/Frontend/ASTConsumers.h"
-#include "clang/Frontend/FrontendActions.h"
 #include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/FrontendActions.h"
+#include "clang/Rewrite/Core/Rewriter.h"
 #include "clang/Tooling/CommonOptionsParser.h"
 #include "clang/Tooling/Tooling.h"
-#include "clang/Rewrite/Core/Rewriter.h"
 
 using namespace std;
 using namespace clang;
@@ -19,25 +19,26 @@ using namespace llvm;
 Rewriter rewriter;
 int numFunctions = 0;
 
-
 class ExampleVisitor : public RecursiveASTVisitor<ExampleVisitor> {
-private:
-    ASTContext *astContext; // used for getting additional AST info
+   private:
+    ASTContext *astContext;  // used for getting additional AST info
 
-public:
-    explicit ExampleVisitor(CompilerInstance *CI) 
-      : astContext(&(CI->getASTContext())) // initialize private members
+   public:
+    explicit ExampleVisitor(CompilerInstance *CI)
+        : astContext(&(CI->getASTContext()))  // initialize private members
     {
-        rewriter.setSourceMgr(astContext->getSourceManager(), astContext->getLangOpts());
+        rewriter.setSourceMgr(astContext->getSourceManager(),
+                              astContext->getLangOpts());
     }
 
     virtual bool VisitFunctionDecl(FunctionDecl *func) {
         numFunctions++;
         string funcName = func->getNameInfo().getName().getAsString();
         if (funcName == "do_math") {
-            rewriter.ReplaceText(func->getLocation(), funcName.length(), "add5");
+            rewriter.ReplaceText(func->getLocation(), funcName.length(),
+                                 "add5");
             errs() << "** Rewrote function def: " << funcName << "\n";
-        }    
+        }
         return true;
     }
 
@@ -45,7 +46,7 @@ public:
         if (ReturnStmt *ret = dyn_cast<ReturnStmt>(st)) {
             rewriter.ReplaceText(ret->getRetValue()->getLocStart(), 6, "val");
             errs() << "** Rewrote ReturnStmt\n";
-        }        
+        }
         if (CallExpr *call = dyn_cast<CallExpr>(st)) {
             rewriter.ReplaceText(call->getLocStart(), 7, "add5");
             errs() << "** Rewrote function call\n";
@@ -53,75 +54,75 @@ public:
         return true;
     }
 
-/*
-    virtual bool VisitReturnStmt(ReturnStmt *ret) {
-        rewriter.ReplaceText(ret->getRetValue()->getLocStart(), 6, "val");
-        errs() << "** Rewrote ReturnStmt\n";
-        return true;
-    }
+    /*
+        virtual bool VisitReturnStmt(ReturnStmt *ret) {
+            rewriter.ReplaceText(ret->getRetValue()->getLocStart(), 6, "val");
+            errs() << "** Rewrote ReturnStmt\n";
+            return true;
+        }
 
-    virtual bool VisitCallExpr(CallExpr *call) {
-        rewriter.ReplaceText(call->getLocStart(), 7, "add5");
-        errs() << "** Rewrote function call\n";
-        return true;
-    }
-*/
+        virtual bool VisitCallExpr(CallExpr *call) {
+            rewriter.ReplaceText(call->getLocStart(), 7, "add5");
+            errs() << "** Rewrote function call\n";
+            return true;
+        }
+    */
 };
 
-
-
 class ExampleASTConsumer : public ASTConsumer {
-private:
-    ExampleVisitor *visitor; // doesn't have to be private
+   private:
+    ExampleVisitor *visitor;  // doesn't have to be private
 
-public:
+   public:
     // override the constructor in order to pass CI
     explicit ExampleASTConsumer(CompilerInstance *CI)
-        : visitor(new ExampleVisitor(CI)) // initialize the visitor
-    { }
+        : visitor(new ExampleVisitor(CI))  // initialize the visitor
+    {}
 
     // override this to call our ExampleVisitor on the entire source file
     virtual void HandleTranslationUnit(ASTContext &Context) {
         /* we can use ASTContext to get the TranslationUnitDecl, which is
-             a single Decl that collectively represents the entire source file */
+             a single Decl that collectively represents the entire source file
+         */
         visitor->TraverseDecl(Context.getTranslationUnitDecl());
     }
 
-/*
-    // override this to call our ExampleVisitor on each top-level Decl
-    virtual bool HandleTopLevelDecl(DeclGroupRef DG) {
-        // a DeclGroupRef may have multiple Decls, so we iterate through each one
-        for (DeclGroupRef::iterator i = DG.begin(), e = DG.end(); i != e; i++) {
-            Decl *D = *i;    
-            visitor->TraverseDecl(D); // recursively visit each AST node in Decl "D"
+    /*
+        // override this to call our ExampleVisitor on each top-level Decl
+        virtual bool HandleTopLevelDecl(DeclGroupRef DG) {
+            // a DeclGroupRef may have multiple Decls, so we iterate through
+       each one for (DeclGroupRef::iterator i = DG.begin(), e = DG.end(); i !=
+       e; i++) { Decl *D = *i; visitor->TraverseDecl(D); // recursively visit
+       each AST node in Decl "D"
+            }
+            return true;
         }
-        return true;
-    }
-*/
+    */
 };
-
-
 
 class ExampleFrontendAction : public ASTFrontendAction {
-public:
-    virtual ASTConsumer *CreateASTConsumer(CompilerInstance &CI, StringRef file) {
-        return new ExampleASTConsumer(&CI); // pass CI pointer to ASTConsumer
+   public:
+    virtual std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI,
+                                                           StringRef file) {
+        return std::unique_ptr<ASTConsumer>(
+            new ExampleASTConsumer(&CI));  // pass CI pointer to ASTConsumer
     }
 };
 
-
-
 int main(int argc, const char **argv) {
+    llvm::cl::OptionCategory category("LibToolingExample options");
     // parse the command-line args passed to your code
-    CommonOptionsParser op(argc, argv);        
+    CommonOptionsParser op(argc, argv, category);
     // create a new Clang Tool instance (a LibTooling environment)
     ClangTool Tool(op.getCompilations(), op.getSourcePathList());
 
     // run the Clang Tool, creating a new FrontendAction (explained below)
-    int result = Tool.run(newFrontendActionFactory<ExampleFrontendAction>());
+    int result =
+        Tool.run(newFrontendActionFactory<ExampleFrontendAction>().get());
 
     errs() << "\nFound " << numFunctions << " functions.\n\n";
     // print out the rewritten source code ("rewriter" is a global var.)
-    rewriter.getEditBuffer(rewriter.getSourceMgr().getMainFileID()).write(errs());
+    rewriter.getEditBuffer(rewriter.getSourceMgr().getMainFileID())
+        .write(errs());
     return result;
 }
--- README.md
+++ README.md
@@ -1,17 +1,19 @@
 # Clang LibTooling Example
 
-This repository includes the companion files for this [Clang LibTooling Example](http://kevinaboos.blogspot.com/2013/07/clang-tutorial-part-ii-libtooling.html).
+To build this repository under Ubuntu 18.04, you need to do the following:
 
-To use these files, first download and build LLVM and Clang from source. 
-
-Then checkout this repository into Clang's tools directory:
-
-	$ cd llvm/tools/clang/tools
-	$ git clone https://github.com/kevinaboos/LibToolingExample.git example
+	$ sudo apt-get install clang-7 libclang-7-dev clang-tools-7
+	$ git clone https://github.com/mohamed/LibToolingExample.git
 
 Then build and run the Example.cpp file:
 
-	$ cd example
-	$ make
-	$ chmod +x run_example.sh
-	$ ./run_example.sh
+	$ mkdir build
+  $ cd build
+  $ cmake ..
+  $ cmake --build .
+  $ cmake --buiild . --target test
+
+Or alternatively, you can run the binary as follows:
+
+  $ cd build
+  $ ./LibToolingExample ../test.c --
